d3f4b18a53783fc832d97d112c046f580c6cfb13
Damien Dunn <damienhoward95@gmail.com>
2020-07-13 22:18:18 -0700

    Add test for Packet Decoding from a byte array

--- tests/osdp_packet_test.go
+++ tests/osdp_packet_test.go
@@ -43,6 +43,17 @@ func TestPacketCreationACK(t *testing.T) {
 	require.Equal(t, correctMessage, osdpPacketBytes)
 }
 
+func TestPacketCreationNAK(t *testing.T) {
+	osdpPacket, err := osdp.NewPacket(osdp.REPLY_NAK, 0x00, []byte{0x01}, true)
+	if err != nil {
+		t.Errorf("Unable to Create OSDP Packet")
+	}
+
+	correctMessage := []byte{0x53, 0x00, 0x09, 0x00, 0x04, 0x41, 0x01, 0x07, 0x70}
+	osdpPacketBytes := osdpPacket.ToBytes()
+	require.Equal(t, correctMessage, osdpPacketBytes)
+}
+
 func TestPacketCreationPOLL(t *testing.T) {
 	osdpPacket, err := osdp.NewPacket(osdp.CMD_POLL, 0x65, []byte{}, true)
 	if err != nil {
@@ -56,7 +67,7 @@ func TestPacketCreationPOLL(t *testing.T) {
 
 func TestPacketCreationCardScan(t *testing.T) {
 	card := []byte("00000000010011100011010101")
-	osdpPacket, err := osdp.NewPacket(osdp.CMD_POLL, 0x65, card, true)
+	osdpPacket, err := osdp.NewPacket(osdp.REPLY_RAW, 0x00, card, true)
 	if err != nil {
 		t.Errorf("Unable to Create OSDP Packet")
 	}
@@ -67,4 +78,39 @@ func TestPacketCreationCardScan(t *testing.T) {
 		0x31, 0x30, 0x31, 0x30, 0x31, 0xFE, 0x40}
 	osdpPacketBytes := osdpPacket.ToBytes()
 	require.Equal(t, correctMessage, osdpPacketBytes)
+}
+
+func TestPacketDecodeACK(t *testing.T) {
+
+	msgToDecode := []byte{0x53, 0x00, 0x08, 0x00, 0x04, 0x40, 0x89, 0x8E}
+	decodedPacket, err := osdp.NewPacketFromBytes(msgToDecode)
+	if err != nil {
+		t.Errorf("Unable to Decode OSDP Packet")
+	}
+
+	correctPacket, err := osdp.NewPacket(osdp.REPLY_ACK, 0x00, []byte{}, true)
+	if err != nil {
+		t.Errorf("Unable to Create OSDP Packet")
+	}
+
+	require.Equal(t, correctPacket, decodedPacket)
+}
+
+func TestPacketDecodeCardScan(t *testing.T) {
+
+	msgToDecode := []byte{0x53, 0x00, 0x22, 0x00, 0x04, 0x50, 0x30, 0x30, 0x30,
+		0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x31, 0x30, 0x30,
+		0x31, 0x31, 0x31, 0x30, 0x30, 0x30, 0x31, 0x31, 0x30,
+		0x31, 0x30, 0x31, 0x30, 0x31, 0xFE, 0x40}
+	decodedPacket, err := osdp.NewPacketFromBytes(msgToDecode)
+	if err != nil {
+		t.Errorf("Unable to Decode OSDP Packet")
+	}
+	card := []byte("00000000010011100011010101")
+	correctPacket, err := osdp.NewPacket(osdp.REPLY_RAW, 0x00, card, true)
+	if err != nil {
+		t.Errorf("Unable to Create OSDP Packet")
+	}
+
+	require.Equal(t, correctPacket, decodedPacket)
 }
